26c74d9f8ce1740d4e35be6608b4ed1b0ecec3b6
Alex <alburton32@gmail.com>
2025-03-25 13:32:36 +0500

    create stimulus clipboard_controller

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="clipboard"
+export default class extends Controller {
+  static targets = [ "source" ]
+
+  copy(){
+    navigator.clipboard.writeText(this.sourceTarget.value)
+  }
+
+  //v2 - copy action attached to <a> link, input from a <textarea>
+  //   Сигнатура "(commandId: string, showUI?: boolean | undefined, value?: string | undefined): boolean" "document.execCommand" устарела.ts(6387)
+  // lib.dom.d.ts(7848, 8): Объявление было отмечено как устаревшее.
+  // copy(event) {
+  //   event.preventDefault()
+  //   this.sourceTarget.select()
+  //   document.execCommand("copy")
+  // }
+}
